3b6c39f5e6b4b46fe70da98eca5e18e3a8e60e1a
DeeGemini <zodiactheegemini@gmail.com>
2025-03-08 23:42:28 +0200

    A bit of background about me as A software engineer

---
+++ components/about.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const About = () => {
+    return (
+      <section className="glass py-20 rounded-lg shadow-lg mx-4 my-8">
+        <h2 className="text-3xl font-bold mb-8 text-center">About Me</h2>
+        <div className="max-w-2xl mx-auto">
+          <p className="mb-4">
+            I'm Nontuthuzelo Ngwenya, a software engineer based in Durban, South Africa. I enjoy creating websites that
+            are both beautiful and responsive.
+          </p>
+          <p className="mb-4">
+            I have a passion for solving real-life problems through code and bringing the solution(s) to life.
+          </p>
+          <p className="mb-4">I have experience in HTML, CSS, JavaScript, Node.js, Typescript, Vue.js, MongoDB, Postman and AI Tools.</p>
+          <p className="mb-4">I am always looking to learn new technologies and improve my skills.</p>
+          <p>I am a hardworking and dedicated individual who is always looking to grow as a developer.</p>
+        </div>
+      </section>
+    )
+  }
+  
+  export default About;
+    
